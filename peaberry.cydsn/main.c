845175ffd9650ae2a10d17874493f35ee4255083
David Turnbull <dturnbull@gmail.com>
2012-04-18 14:06:59 -0700

    first commit

---
+++ peaberry.cydsn/main.c
@@ -0,0 +1,25 @@
+/* ========================================
+ *
+ * Copyright 2012 David Turnbull
+ * All Rights Reserved
+ * UNPUBLISHED, LICENSED SOFTWARE.
+ *
+ * CONFIDENTIAL AND PROPRIETARY INFORMATION
+ * WHICH IS THE PROPERTY OF DAVID TURNBULL.
+ *
+ * ========================================
+*/
+#include <device.h>
+
+void main()
+{
+    /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+
+    /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
+    for(;;)
+    {
+        /* Place your application code here. */
+    }
+}
+
+/* [] END OF FILE */
