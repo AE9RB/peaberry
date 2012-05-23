f95268a0d579403449d41aa7e2f4c87be5c49e56
David Turnbull <dturnbull@gmail.com>
2012-05-23 12:04:17 -0700

    USB Audio Enumerating

--- peaberry.cydsn/main.c
+++ peaberry.cydsn/main.c
@@ -14,11 +14,25 @@
 void main()
 {
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
+    CyGlobalIntEnable;
+    USBFS_Start(0, USBFS_5V_OPERATION);
+    while(!USBFS_GetConfiguration());
 
     /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
     for(;;)
     {
-        /* Place your application code here. */
+	    if(USBFS_IsConfigurationChanged() != 0u) {
+            uint8 conf = USBFS_GetConfiguration();
+            if (conf != 0u) {
+                uint8 intset = USBFS_GetInterfaceSetting(1u);
+                if (intset == 1u) {
+                    // USBFS_EnableOutEP(2u);
+                } else {
+                    // USBFS_DisableOutEP(2u);
+                }
+            }    
+        }
+
     }
 }
 
