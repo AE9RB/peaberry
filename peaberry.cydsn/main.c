1c147e8f5d3372ec583269d04ac6966e6f6d0744
David Turnbull <dturnbull@gmail.com>
2012-05-24 20:53:21 -0700

    pcm3060 enabled

--- peaberry.cydsn/main.c
+++ peaberry.cydsn/main.c
@@ -11,16 +11,37 @@
 */
 #include <device.h>
 
+#define PCM3060_ADDR 0x46
+#define PCM3060_REG 0x40
+uint8 pcm3060_enable[2];
+
+
 void main()
 {
+    pcm3060_enable[0] = 0x40;
+    pcm3060_enable[1] = 0xC0;
+
     /* Place your initialization/startup code here (e.g. MyInst_Start()) */
     CyGlobalIntEnable;
+    
     USBFS_Start(0, USBFS_5V_OPERATION);
-    while(!USBFS_GetConfiguration());
+
+    I2S_Start();
+
+    I2C_Start();
+    I2C_MasterClearStatus();
+    I2C_MasterWriteBuf(PCM3060_ADDR, (uint8 *) pcm3060_enable, 2, I2C_MODE_COMPLETE_XFER);
+    while(0u == (I2C_MasterStatus() & I2C_MSTAT_WR_CMPLT)) {}
+    
+    //while(!USBFS_GetConfiguration());
+    
 
     /* CyGlobalIntEnable; */ /* Uncomment this line to enable global interrupts. */
     for(;;)
     {
+    
+        
+    
 	    if(USBFS_IsConfigurationChanged() != 0u) {
             uint8 conf = USBFS_GetConfiguration();
             if (conf != 0u) {
