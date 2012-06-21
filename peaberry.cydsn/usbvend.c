874e7101d557ad21da15346208c3094c957040dd
David Turnbull <dturnbull@gmail.com>
2012-06-20 19:57:42 -0700

    transmit working

--- peaberry.cydsn/usbvend.c
+++ peaberry.cydsn/usbvend.c
@@ -10,7 +10,9 @@
  * ========================================
 */
 
-#include "USBFS.h"
+#include "device.h"
+//#include "USBFS.h"
+#include "main.h"
 #include "si570.h"
 
 uint8 USBFS_InitControlRead(void);
@@ -18,10 +20,14 @@ uint8 USBFS_InitControlWrite(void);
 extern volatile T_USBFS_TD USBFS_currentTD;
 extern volatile uint32 Si570_LO;
 
+uint8 key_return;
+
 uint8 USBFS_HandleVendorRqst(void) 
 {
     uint8 requestHandled = USBFS_FALSE;
     uint8 reqType, reqCmd;
+    
+    uint8 test1, test2;
     
     reqType = CY_GET_REG8(USBFS_bmRequestType);
     reqCmd = CY_GET_REG8(USBFS_bRequest);
@@ -33,6 +39,18 @@ uint8 USBFS_HandleVendorRqst(void)
             case 0x3A: // CMD_GET_FREQ
                 USBFS_currentTD.pData = (void *)&Si570_LO;
                 USBFS_currentTD.count = sizeof(Si570_LO);
+                requestHandled  = USBFS_InitControlRead();
+            case 0x50: // CMD_SET_USRP1
+                if (CY_GET_REG8(USBFS_wValueLo) & 0x01) {
+                    Control_Write(Control_Read() | CONTROL_TX_ENABLE);
+                } else {
+                    Control_Write(Control_Read() & ~CONTROL_TX_ENABLE);
+                }
+                //nobreak, returns key value
+            case 0x51: // CMD_GET_CW_KEY
+                key_return = 0;//TODO convert key bits
+                USBFS_currentTD.pData = (void *)&key_return;
+                USBFS_currentTD.count = sizeof(key_return);
                 requestHandled  = USBFS_InitControlRead();
             default:
                 break;
