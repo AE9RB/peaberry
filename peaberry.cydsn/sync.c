8408e754c8cc0c69511fc2b5ad4fc931f214f9af
David Turnbull <dturnbull@gmail.com>
2015-03-10 09:28:35 -0700

    Detect and correct unusual PLL lock condition

--- peaberry.cydsn/sync.c
+++ peaberry.cydsn/sync.c
@@ -17,10 +17,11 @@
 #define SYNC_SOF_CENTER 22000
 #define SYNC_FRAC_MIN (FracN_DEFAULT-1000)
 #define SYNC_FRAC_MAX (FracN_DEFAULT+500)
-#define SYNC_I_GUARD 3000
-#define SYNC_P_GAIN 0.0001
-#define SYNC_I_GAIN 0.001
+#define SYNC_P_GAIN 0.0004
 #define SYNC_D_GAIN 0.5
+
+#define PLL_VERIFY_TIME 3000
+#define PLL_RESET_TIME 200
 
 void Sync_Start(void) {
     FracN_Start(P_DMA);
@@ -31,31 +32,61 @@ void Sync_Start(void) {
 
 void Sync_Main(void) {
     static uint16 frac = FracN_DEFAULT;
-    static int16 prev_error = 0, int_error = 0;
-    int16 cur_error, diff, p_term, i_term, d_term;
+    static int16 prev_error = 0;
+    
+    static uint8 pll_resetting = 0;
+    static uint16 pll_testing = PLL_VERIFY_TIME;
+    static uint16 pll_diff_acc = 0, pll_error_count = 0;
+    
+    int16 cur_error, diff, p_term, d_term;
     uint16 pos;
+    
     pos = CY_GET_REG8(SyncSOF_FRAME_POS_LO__STATUS_REG);
     if (pos & 0x01) {
         pos += (uint16)CY_GET_REG8(SyncSOF_FRAME_POS_HI__STATUS_REG) << 8;
 
         cur_error = pos - SYNC_SOF_CENTER;
-        int_error += cur_error;
-        if (int_error < -SYNC_I_GUARD)
-            int_error = -SYNC_I_GUARD;
-        else if (int_error > SYNC_I_GUARD)
-            int_error = SYNC_I_GUARD;
             
         diff = cur_error - prev_error;
         p_term = cur_error * SYNC_P_GAIN;
-        i_term = int_error * SYNC_I_GAIN;
         d_term = diff * SYNC_D_GAIN;
         prev_error = cur_error;
-        int_error *= 0.9;
         
-        frac += p_term + i_term + d_term;
+        frac += p_term + d_term;
+        
+        if (frac > SYNC_FRAC_MAX) frac = SYNC_FRAC_MAX;
+        if (frac < SYNC_FRAC_MIN) frac = SYNC_FRAC_MIN;
+        
+        if (pll_testing) {
+            if (pll_resetting) {
+                frac = 0;
+                // Wait for PLL reset
+                if (!(--pll_resetting)) {
+                    Control_Write(Control_Read() & ~CONTROL_LED);
+                    frac = FracN_DEFAULT;
+                    FRAC_CLK_SetDividerValue(13);
+                }
+            } else {
+                pll_testing--;
+                if (diff < 0) pll_diff_acc -= diff;
+                else pll_diff_acc += diff;
+                pll_diff_acc /= 2;
+                if (pll_diff_acc < 2) {
+                    if (pll_error_count) pll_error_count--;
+                } else if (pll_diff_acc > 7) {
+                    pll_error_count++;
+                }
+                if (pll_error_count > 50) {
+                    pll_diff_acc = 0;
+                    pll_error_count = 0;
+                    pll_resetting = PLL_RESET_TIME;
+                    pll_testing = PLL_VERIFY_TIME;
+                    Control_Write(Control_Read() | CONTROL_LED);
+                    FRAC_CLK_SetDividerValue(14);
+                }
+            }
+        }
 
-        if (frac > SYNC_FRAC_MAX) frac = FracN_DEFAULT;
-        if (frac < SYNC_FRAC_MIN) frac = FracN_DEFAULT;
         FracN_Set(frac);
     }
 }
